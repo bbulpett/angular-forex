9b922cb0f7594b386645f00299d145e3b685736c
Barnabas Bulpett <bbulpett@charter.net>
2015-11-07 17:38:50 -0500

    Correct jsonp parse and render

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # angular-forex
 Simple AngularJS app that pulls live JSON updates of forex data from remote domain
+
+Set up to use data from Yahoo Finance. Thanks to them for that.
--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-var App = angular.module('App', []);
+var app = angular.module('app', []);
 
-App.controller('ForexCtrl', function ($scope, $http) {
-    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=jsonp&callback=JSON_CALLBACK').success(function (data) {
+app.controller('DataCtrl', function ($scope, $http) {
+    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=JSON_CALLBACK').success(function (data) {
+
         $scope.data = data;
+        $scope.resources = data.query.results.list.resources;
+
+        // if ( angular.isArray(data.results.list.resources.resource) ) {
+        // 	$scope.results = data.results.list.resources.resource;
+        // }
+        // else {
+        // 	$scope.results = [data.results.list.resources.resource];
+        // }
     });
 });
-
 
 // var App = angular.module('App', []);
 
