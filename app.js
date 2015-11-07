09e2b6e53412a3e77d3e72e62de8ed48b8cfc6ff
Barnabas Bulpett <bbulpett@charter.net>
2015-11-06 22:49:15 -0500

    Now using jsonp() and proper url with JSON_CALLBACK

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var App = angular.module('App', []);
 
-App.controller('ScoreboardCtrl', function($scope, $http) {
-  $http.jsonp('http://data.nba.com/data//json/nbacom/2015/gameline/20151012/games.json')
-    .then(function(res){
-      // Storing the json data object as 'scores'
-      $scope.games = res.data;
+App.controller('ForexCtrl', function ($scope, $http) {
+    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=jsonp&callback=JSON_CALLBACK').success(function (data) {
+        $scope.data = data;
     });
 });
+
+// var App = angular.module('App', []);
+
+// function jsonp_callback(data) {
+// 	console.log(data.found);
+// }
+
+// var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=jsonp_callback";
+
+// App.controller('ScoreboardCtrl', function($scope, $http) {
+//   $http.jsonp(url);
+// });
