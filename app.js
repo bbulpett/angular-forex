33c9ecaab00155f63941136d676cdff39014df79
Barnabas Bulpett <bbulpett@charter.net>
2015-11-08 10:01:51 -0500

    Create object from data set, reference in view

--- app.js
+++ app.js
@@ -2,27 +2,14 @@ var app = angular.module('app', []);
 
 app.controller('DataCtrl', function ($scope, $http) {
     $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=JSON_CALLBACK').success(function (data) {
-
-        $scope.data = data;
-        $scope.resources = data.query.results.list.resources;
-
-        // if ( angular.isArray(data.results.list.resources.resource) ) {
-        // 	$scope.results = data.results.list.resources.resource;
-        // }
-        // else {
-        // 	$scope.results = [data.results.list.resources.resource];
-        // }
+		    $scope.data = data;
+        // $scope.resources = data.query.results.list.resources;
+		    $scope.elements = $scope.data.query.results.list.resources.map(function (res) {
+				  var e = {};
+				  e.name = res.resource.fields.name;
+				  e.price = res.resource.fields.price;
+				  e.utctime = res.resource.fields.utctime;
+				  return e;
+			});
     });
 });
-
-// var App = angular.module('App', []);
-
-// function jsonp_callback(data) {
-// 	console.log(data.found);
-// }
-
-// var url = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=jsonp_callback";
-
-// App.controller('ScoreboardCtrl', function($scope, $http) {
-//   $http.jsonp(url);
-// });
