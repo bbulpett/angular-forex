abc78c073eed952baf70f7d20e867fb53bf9e9f0
Barnabas Bulpett <bbulpett@charter.net>
2015-11-09 12:16:27 -0500

    Add 1s interval function

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
 var app = angular.module('app', []);
 
-app.controller('DataCtrl', function ($scope, $http) {
-    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=JSON_CALLBACK').success(function (data) {
-		    $scope.data = data;
-        // $scope.resources = data.query.results.list.resources;
-		    $scope.elements = $scope.data.query.results.list.resources.map(function (res) {
-				  var e = {};
-				  e.name = res.resource.fields.name;
-				  e.price = res.resource.fields.price;
-				  e.utctime = res.resource.fields.utctime;
+app.controller('DataCtrl', function ($scope, $http, $timeout) {
 
-				  return e;
+
+	$scope.getData = function() {
+	    $http.jsonp('https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20json%20where%20url%3D%22http%3A%2F%2Ffinance.yahoo.com%2Fwebservice%2Fv1%2Fsymbols%2Fallcurrencies%2Fquote%3Fformat%3Djson%22&format=json&diagnostics=true&callback=JSON_CALLBACK').success(function (data) {
+			    $scope.data = data;
+	        // $scope.resources = data.query.results.list.resources;
+			    $scope.elements = $scope.data.query.results.list.resources.map(function (res) {
+					  var e = {};
+					  e.name = res.resource.fields.name;
+					  e.price = res.resource.fields.price;
+					  e.utctime = res.resource.fields.utctime;
+	
+					  return e;
+				});
 			});
-    });
+		};
+
+		$scope.intervalFunction = function(){
+			$timeout(function() {
+				$scope.getData();
+				$scope.intervalFunction();
+			}, 1000)
+		};
+    $scope.intervalFunction();
 });
 
 app.controller('TimeCtrl', function($scope, $interval) {
@@ -21,4 +33,4 @@ app.controller('TimeCtrl', function($scope, $interval) {
 	}
 	tick();
 	$interval(tick, 1000);
-})
+});
