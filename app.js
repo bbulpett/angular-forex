1d9b96846f6c1623903c8083cc9025aca7188425
Barnabas Bulpett <bbulpett@charter.net>
2015-11-11 23:53:08 -0500

    Function to return market session from switch-range statement

--- app.js
+++ app.js
@@ -40,4 +40,30 @@ app.controller('TimeCtrl', function($scope, $interval) {
 	}
 	tick();
 	$interval(tick, 1000);
+
+	var utc = new Date().getUTCHours();
+	var getMktSession = function(utc) {
+
+		switch(true) {
+			case ( 22 <= utc || utc < 7):
+				return "Sydney";
+			case ( 22 <= utc || utc < 6):
+				return "Wellington";
+			case ( 0 <= utc < 9):
+				return "Tokyo";
+			case ( 1 <= utc < 10):
+				return "Hong Kong";
+			case ( 7 <= utc < 16):
+				return "Frankfurt";
+			case ( 8 <= utc < 17):
+				return "London";
+			case ( 13 <= utc < 22):
+				return "New York";
+			case ( 14 <= utc < 23):
+				return "Chicago";
+		}
+	}
+	$scope.mkt_session = getMktSession(utc);
+	$interval(getMktSession, 60000);
 });
+
